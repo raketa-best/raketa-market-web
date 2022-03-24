d1f27f325747a5c983cdcd544d2032a3597c1bed
WhiteBox <artgroop.whitebox@gmail.com>
2022-03-24 20:48:04 +0300

    add const layoutFixedPos :star: #19

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from 'react'
 import NavBar from '../navBar/NavBar'
 import classes from './Layout.module.css'
 
 export interface ILayoutProps { 
-
     component: React.ReactElement<any>
 }
 
 const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
+
+    const [styleFixed, setStyleFixed] = useState<string>('')
+    console.log('styleFixed', styleFixed)
+
+    useEffect(() => {
+        setStyleFixed('')
+        window.scroll(0, 0)
+    }, [])
+    
+    const layoutFixedPos = () => {
+        let scrollTop = window.scrollY
+        console.log('scrollTop', scrollTop)
+        if (scrollTop >= 300) {
+            setStyleFixed(classes.layout_fixed_pos)
+        } else {
+            setStyleFixed('')
+        }
+    }
+
+    window.addEventListener('scroll', () => {
+        layoutFixedPos()
+    }) 
+
     return <div className={classes.layout_block}>
-        <div className={classes.layout}>
+        <div className={`${classes.layout} ${styleFixed}`}>
             <div className={classes.component}>
                 {props.component}
             </div>
